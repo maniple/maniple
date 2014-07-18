4db3be3c9976ddae75cb1d00ed5cc0e0245ece89
xemlock <xemlock@gmail.com>
2014-07-18 16:24:29 +0200

    added support for recursive onject/resource instantiation in 'params' array

--- library/Maniple/Application/ResourceContainer.php
+++ library/Maniple/Application/ResourceContainer.php
@@ -46,48 +46,48 @@ class Maniple_Application_ResourceContainer
      * Add a resource
      *
      * @param  string $name
-     * @param  string|array|object $service
+     * @param  string|array|object $resource
      * @return Maniple_Application_ResourceContainer
      */
-    public function addResource($name, $service) // {{{
+    public function addResource($name, $resource) // {{{
     {
         switch (true) {
-            case is_string($service):
-                if (!strncasecmp($service, 'resource:', 9)) {
-                    $service = new Maniple_Application_ResourceAlias(substr($service, 9));
+            case is_string($resource):
+                if (!strncasecmp($resource, 'resource:', 9)) {
+                    $resource = new Maniple_Application_ResourceAlias(substr($resource, 9));
                 } else {
                     // string not begining with 'resource:' is considered to be
-                    // a resource's class name
-                    $service = array(
-                        'class' => $service,
+                    // a class name
+                    $resource = array(
+                        'class' => $resource,
                         'params' => null,
                     );
                 }
                 break;
 
-            case is_array($service):
+            case is_array($resource):
                 // if a 'class' key is detected service is assumed to be
                 // a service definition
-                if (isset($service['class'])) {
-                    $service = array_merge(array('params' => null), $service);
+                if (isset($resource['class'])) {
+                    $resource = array_merge(array('params' => null), $resource);
                 }
                 break;
         }
 
-        $serviceName = strtolower($name);
+        $resourceName = strtolower($name);
 
-        if (isset($this->_resources[$serviceName])) {
+        if (isset($this->_resources[$resourceName])) {
             throw new Exception(sprintf(
                 "Resource '%s' is already registered", $name
             ));
         }
 
-        $this->_resources[$serviceName] = $service;
+        $this->_resources[$resourceName] = $resource;
         return $this;
     } // }}}
 
     /**
-     * Retrieve a service instance
+     * Retrieve a resource instance
      *
      * @param  string $name
      * @return mixed
@@ -103,11 +103,13 @@ class Maniple_Application_ResourceContainer
             switch (true) {
                 case $resource instanceof Maniple_Application_ResourceAlias:
                     // resolve resource alias
-                    // TODO cycle detection
-                    return $this->getResource($resource->getTarget());
+                    return $this->_resources[$resourceName] = $this->getResource($resource->getTarget());
 
                 case is_array($resource) && isset($resource['class']):
-                    return $this->_resources[$resourceName] = $this->_loadResource($resource);
+                    $resourceClass = $resource['class'];
+                    $resourceParams = isset($resource['params']) ? $resource['params'] : null;
+                    $resourceInstance = $this->createInstance($resourceClass, $resourceParams);
+                    return $this->_resources[$resourceName] = $resourceInstance;
 
                 default:
                     return $resource;
@@ -118,21 +120,14 @@ class Maniple_Application_ResourceContainer
     } // }}}
 
     /**
-     * Create a resource instance from definition.
+     * Create an instance of a given class and setup its parameters.
      *
-     * @param  array $service
-     * @return mixed
-     * @throws Exception
+     * @param  string $class
+     * @param  array $params OPTIONAL
+     * @return object
      */
-    protected function _loadResource(array $service) // {{{
+    public function createInstance($class, $params = null) // {{{
     {
-        if (empty($service['class'])) {
-            throw new Exception('No resource class name provided');
-        }
-
-        $class = $service['class'];
-        $params = $service['params'];
-
         // replace service placeholders with service instances
         if (is_object($params) && method_exists($params, 'toArray')) {
             $params = $params->toArray();
@@ -140,35 +135,42 @@ class Maniple_Application_ResourceContainer
 
         $params = (array) $params;
 
-        // no cycle check for now
+        // @TODO maybe some cycle check when instantiating resources?
         foreach ($params as $key => $value) {
-            if (is_string($value) && !strncasecmp($value, 'service:', 8)) {
-                $params[$key] = $this->getResource(substr($value, 8));
-            }
             if (is_string($value) && !strncasecmp($value, 'resource:', 9)) {
                 $params[$key] = $this->getResource(substr($value, 9));
             }
+            // recursively replace arrays with 'class' key with instances of
+            // matching classes
+            if (is_array($value) && isset($value['class'])) {
+                $valueClass = $value['class'];
+                $valueParams = isset($value['params']) ? $value['params'] : null;
+                $params[$key] = $this->createInstance($valueClass, $valueParams);
+            }
         }
 
-        $service = new $class($this, $params); // This way of service initialization is now deprecated
-        $filter = new Zend_Filter_Word_UnderscoreToCamelCase();
+        $instance = new $class();
 
+        // Set parameters using setter methods, try camel-cased versions
+        // first, then underscored. Because PHP is case-insensitive when
+        // it comes to function names, there is no need to appy some fancy
+        // underscore-to-camel-case filter. Removing all underscore is
+        // sufficient.
         foreach ($params as $key => $value) {
             $methods = array(
-                'set' . $filter->filter($key),
+                'set' . str_replace('_', '', $key),
                 'set' . $key
             );
             foreach ($methods as $method) {
-                if (method_exists($service, $method)) {
-                    $service->{$method}($value);
+                if (method_exists($instance, $method)) {
+                    $instance->{$method}($value);
                     break;
                 }
             }
         }
 
-        return $service;
+        return $instance;
     } // }}}
-
 
     /**
      * Proxy to {@see getResource()}.
@@ -189,15 +191,15 @@ class Maniple_Application_ResourceContainer
      * This function is expected to be called by Bootstrap.
      *
      * @param  string $name
-     * @param  mixed $service
+     * @param  mixed $resource
      */
-    public function __set($name, $service) // {{{
+    public function __set($name, $resource) // {{{
     {
-        return $this->addResource($name, $service);
+        return $this->addResource($name, $resource);
     } // }}}
 
     /**
-     * Is service of a given name defined.
+     * Is resource of a given name defined.
      *
      * This function is expected to called by Bootstrap.
      *
