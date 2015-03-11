5edcf290cf59272e367fc7f45dca8e8eeef990e7
xemlock <xemlock@gmail.com>
2015-03-11 23:35:52 +0100

    _loadPluginResource impl moved to zefram

--- library/Maniple/Application/Bootstrap/Bootstrap.php
+++ library/Maniple/Application/Bootstrap/Bootstrap.php
@@ -24,39 +24,6 @@ class Maniple_Application_Bootstrap_Bootstrap
     } // }}}
 
     /**
-     * Loads a plugin resource.
-     *
-     * If a 'class' option is provided, a lazy resource is instantiated
-     * instead of a plugin resource. To override this behavior (e.g. when
-     * 'class' is a valid option for a plugin resource), add 'plugin' option
-     * with a truthy value.
-     *
-     * Lazy resources, in order to work as intended, must be supported by
-     * the resource container the bootstrap operates on.
-     *
-     * @param  string $resource
-     * @param  array|object|null $options
-     * @return string|false
-     */
-    protected function _loadPluginResource($resource, $options = null) // {{{
-    {
-        if (is_object($options) && method_exists($options, 'toArray')) {
-            $options = $options->toArray();
-        }
-        $options = (array) $options;
-
-        if (isset($options['class']) && empty($options['plugin'])) {
-            $result = strtolower($resource);
-            $this->_pluginResources[$result] = new Maniple_Application_Resource_LazyResource($options);
-        } else {
-            unset($options['plugin']);
-            $result = parent::_loadPluginResource($resource, $options);
-        }
-
-        return $result;
-    } // }}}
-
-    /**
      * Bootstrap module.
      *
      * @deprecated
