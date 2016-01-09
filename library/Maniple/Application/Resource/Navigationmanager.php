8a9f29b462eab0d27385932496efc31c34789c86
xemlock <xemlock@gmail.com>
2016-01-09 13:21:12 +0100

    added Navigation_Manager and Navigationmanager resource

---
+++ library/Maniple/Application/Resource/Navigationmanager.php
@@ -0,0 +1,25 @@
+<?php
+
+class Maniple_Application_Resource_NavigationManager extends Zend_Application_Resource_ResourceAbstract
+{
+    /**
+     * @var Maniple_Navigation_Manager
+     */
+    protected $_manager;
+
+    /**
+     * @return Maniple_Navigation_Manager
+     */
+    public function getNavigationManager()
+    {
+        if (null === $this->_manager) {
+            $this->_manager = new Maniple_Navigation_Manager();
+        }
+        return $this->_manager;
+    }
+
+    public function init()
+    {
+        return $this->getNavigationManager();
+    }
+}
---
+++ library/Maniple/Navigation/Manager.php
@@ -0,0 +1,63 @@
+<?php
+
+class Maniple_Navigation_Manager
+{
+    /**
+     * Array of navigation containers
+     *
+     * @var array
+     */
+    protected $_containers = array();
+
+    /**
+     * @param string $name
+     * @param array|Zend_Navigation_Container|Zend_Config $container OPTIONAL
+     * @return Maniple_Navigation_Manager
+     * @throws Zend_Navigation_Exception
+     */
+    public function setContainer($name, $container = null)
+    {
+        switch (true) {
+            case null === $container:
+            case is_array($container):
+            case $container instanceof Zend_Navigation_Container:
+            case $container instanceof Zend_Config:
+                $this->_containers[$name] = $container;
+                break;
+
+            default:
+                throw new Zend_Navigation_Exception(sprintf(
+                    'Navigation must be an array or an instance of Zend_Navigation_Container or Zend_Config, %s given',
+                    is_object($container) ? get_class($container) : gettype($container)
+                ));
+        }
+        return $this;
+    }
+
+    /**
+     * @param string $name
+     * @return Zend_Navigation_Container
+     * @throws Zend_Navigation_Exception
+     */
+    public function getContainer($name)
+    {
+        if ($this->hasContainer($name)) {
+            $container = $this->_containers[$name];
+            if (!$container instanceof Zend_Navigation_Container) {
+                $container = new Zend_Navigation($container);
+                $this->_containers[$name] = $container;
+            }
+            return $container;
+        }
+        throw new Zend_Navigation_Exception(sprintf('Invalid navigation container name: %s', $name));
+    }
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function hasContainer($name)
+    {
+        return isset($this->_containers[$name]) || array_key_exists($name, $this->_containers);
+    }
+}
