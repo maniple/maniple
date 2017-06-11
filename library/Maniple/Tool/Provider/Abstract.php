fadc029eb0950bc1825b29be43303f4aba0b155d
xemlock <xemlock@gmail.com>
2017-06-11 16:28:58 +0200

    Make getApplication() protected to not appear in provider's actions on help screen

--- library/Maniple/Tool/Provider/Abstract.php
+++ library/Maniple/Tool/Provider/Abstract.php
@@ -3,16 +3,18 @@
 class Maniple_Tool_Provider_Abstract extends Zend_Tool_Framework_Provider_Abstract
 {
     /**
+     * Retrieve application instance from client
+     *
      * @return Zend_Application
      * @throws Exception
      */
-    function getApplication()
+    protected function _getApplication()
     {
         $client = $this->_registry->getClient();
         $application = method_exists($client, 'getApplication') ? $client->getApplication() : null;
 
         if (!$application instanceof Zend_Application) {
-            throw new Exception('Unable to retrieve Zend_Application');
+            throw new Zend_Tool_Framework_Provider_Exception('Unable to retrieve Zend_Application from client');
         }
 
         return $application;
