5dc9b87f3e8f0c4e11a9c0bb3ac2858945d16343
xemlock <xemlock@gmail.com>
2019-03-10 13:27:34 +0100

    Don't reference asset view helper in moduleAsset

--- library/Maniple/View/Helper/ModuleAsset.php
+++ library/Maniple/View/Helper/ModuleAsset.php
@@ -5,9 +5,19 @@
  */
 class Maniple_View_Helper_ModuleAsset extends Zend_View_Helper_Abstract
 {
+    /**
+     * @return Maniple_AssetMananger_Service
+     */
+    protected function _getAssetManager()
+    {
+        /** @var Maniple_AssetMananger_Service $assetManager */
+        $assetManager = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getResource('Maniple.AssetManager');
+        return $assetManager;
+    }
+
     public function moduleAsset($path, $moduleName = null)
     {
-        return $this->view->asset($path, $moduleName);
+        return $this->_getAssetManager()->getAssetUrl($path, $moduleName);
     }
 
     public function addManifest(array $manifest, $moduleName)
