33f3ddc08bfc0082e3cc3b28b6dcda753a817919
xemlock <952555+xemlock@users.noreply.github.com>
2019-07-14 22:31:59 +0200

    Fix TableProvider::getTable() impl

--- library/Maniple/Model/TableProvider.php
+++ library/Maniple/Model/TableProvider.php
@@ -16,15 +16,15 @@
 class Maniple_Model_TableProvider
 {
     /**
-     * @param Maniple_Di_Container $container
      * @param string $tableName
+     * @param Maniple_Di_Container $container
      * @return Zefram_Db_Table|Zend_Db_Table_Abstract
      * @throws Zend_Application_Exception
      */
-    public static function getTable(Maniple_Di_Container $container, $tableName)
+    public static function getTable($tableName, Maniple_Di_Container $container)
     {
         /** @var Zefram_Db $db */
         $db = $container->getResource('Zefram_Db');
-        return $db->getTable($name);
+        return $db->getTable($tableName);
     }
 }
