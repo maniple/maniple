2585c6b817a4d5e177890c048fe47b93e81540cb
xemlock <952555+xemlock@users.noreply.github.com>
2019-08-10 16:51:46 +0200

    Add abstract viewHelper with auto-injection

---
+++ library/Maniple/View/Helper/Abstract.php
@@ -0,0 +1,11 @@
+<?php
+
+class Maniple_View_Helper_Abstract extends Zend_View_Helper_Abstract
+{
+    public function __construct()
+    {
+        /** @var Maniple_Di_Injector $injector */
+        $injector = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getResource('Maniple.Injector');
+        $injector->inject($this);
+    }
+}
