e4d58122f61e872b2508231ecc0b4d9a71a3f311
xemlock <952555+xemlock@users.noreply.github.com>
2019-08-15 22:30:33 +0200

    Simplify DbTable slug generator

--- library/Maniple/SlugGenerator/DbTable.php
+++ library/Maniple/SlugGenerator/DbTable.php
@@ -8,21 +8,9 @@ class Maniple_SlugGenerator_DbTable extends Maniple_SlugGenerator_Db
      */
     public function __construct(Zend_Db_Table_Abstract $table, $colName = 'slug')
     {
-        $this->setFromTable($table);
-        $this->setColName($colName);
-    }
-
-    /**
-     * @param Zend_Db_Table_Abstract $table
-     * @return $this
-     */
-    public function setFromTable(Zend_Db_Table_Abstract $table)
-    {
+        $db = $table->getAdapter();
         $tableName = method_exists($table, 'getName') ? $table->getName() : $table->info(Zend_Db_Table::NAME);
 
-        $this->_db = $table->getAdapter();
-        $this->_tableName = $tableName;
-
-        return $this;
+        parent::__construct($db, $tableName, $colName);
     }
 }
